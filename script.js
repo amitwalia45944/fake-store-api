94a9ebad8c989085a814c9ff1dac314fc35f79c1
amit <amit.kumar.25.1@mountblue.tech>
2023-08-23 10:36:05 +0530

    Implemented features when API is loading -> loader will display API did not load -> Error message Products are loaded -> Render the products API successful but no products -> Show a no products message

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const loader = document.getElementById("loader");
+let error_mssg = document.getElementById("error-message");
+
 let url = 'https://fakestoreapi.com/products';
 
 window.addEventListener("load", () => {
@@ -9,42 +12,61 @@ function reload() {
 };
 
 function fetch_item(url) {
+    loader.style.display = "block";
+    error_mssg.textContent = "";
+
     fetch(url)
         .then((response) => {
-            return response.json();
+            if (!response.ok) {
+                throw new Error("API network response problem");
+            } else {
+                return response.json();
+            }
         })
         .then((products) => {
-            bind_data(products);
+            if (products.length > 0) {
 
-            
-                let container_data = document.getElementById("cards-container"); 
+                bind_data(products);
+
+                let container_data = document.getElementById("cards-container");
+
                 container_data.addEventListener("click", (event) => {
 
+                    const target_card = event.target.closest(".card");
 
-                const target_card = event.target.closest(".card");
-                console.log(target_card);
+                    const clicked_product = target_card.querySelector(".product-id");
+                    const pid_of_product = clicked_product.textContent.split(":")[1].trim();
 
-                const clicked_product = target_card.querySelector(".product-id");
-                const pid_of_product = clicked_product.textContent.split(":")[1].trim();
+                    const found_product = products.find((item) => {
+                        return item.id === parseInt(pid_of_product);
+                    });
 
-                const found_product = products.find((item) => {
-                    return item.id === parseInt(pid_of_product);
+                    if (found_product) {
+                        display(found_product);
+
+                    } else {
+                        console.log('not found');
+                    }
+                    container_data.style.display = 'none';
                 });
-
-                if (found_product) {
-                    console.log(found_product);
-                    display(found_product);
-                } else {
-                    console.log('not found');
-                }
-                container_data.style.display = 'none';
-            });
+            } else {
+                show_no_product_message();
+            }
         })
         .catch((error) => {
-            console.log(error);
+            error_mssg.textContent = "Error: " + error.message;
+        })
+        .finally(() => {
+            loader.style.display = "none";
         });
-
 };
+
+function show_no_product_message() {
+
+    const mssg = document.getElementById("no-products-message");
+    mssg.innerHTML = "No Products fetched from api";
+    mssg.style.display = "block";
+}
 
 function bind_data(products) {
     const cards_container = document.getElementById("cards-container");
@@ -84,7 +106,7 @@ function display(product) {
 
     views_container.innerHTML = "";
     const card_clone = product_card_template.content.cloneNode(true);
-    
+
     const found_product_img = card_clone.querySelector("#product-img");
     const found_product_title = card_clone.querySelector("#product-title");
     let found_product_price = card_clone.querySelector("#product-price");
@@ -92,24 +114,25 @@ function display(product) {
     let found_product_desc = card_clone.querySelector("#product-desc");
     found_product_img.src = product.image;
 
-    found_product_id.innerHTML = `Rating: ${product.rating.rate}🌟 `; 
+    found_product_id.innerHTML = `Rating: ${product.rating.rate}🌟 `;
 
     found_product_price.innerHTML = '$' + product.price;
     found_product_title.innerHTML = product.title;
-   
+
     const desc = product.description;
 
     found_product_desc.innerHTML = desc;
 
     views_container.appendChild(card_clone);
-    
+
 };
 
 
 let current_status = null;
 
 function search_item(specific_category) {
-
+    let container_data = document.getElementById("cards-container");
+    container_data.style.display = 'flex';
     let category_url = `${url}/${'category'}/${specific_category}`;
 
     fetch_item(category_url);
